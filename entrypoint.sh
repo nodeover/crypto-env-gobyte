da0e342061a989a6a503338bee11893de17d3762
Alex <alex@kidsnote.com>
2018-06-29 16:09:36 +0900

    minor fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-chown crypto.crypto /home/crypto/gobyte -R
-chown crypto.crypto /home/crypto/.gobytecore -R
+sudo chown crypto.crypto /home/crypto/gobyte -R
+sudo chown crypto.crypto /home/crypto/.gobytecore -R
 
 source /home/crypto/gobyte/venv/bin/activate
 gobyted -daemon
-sudo cron
+sudo cron -f
 echo "[$APP_NAME Server Started]"
 # tail -f /dev/null
