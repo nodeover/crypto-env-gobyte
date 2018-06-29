3df9c3d05ff218fdc58fbca7bfe8982064e8821e
Alex <alex@kidsnote.com>
2018-06-29 16:07:02 +0900

    minor udpates

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-sudo /home/crypto/gobyte/venv/bin gobyted -daemon
-sudo cron -f
+chown crypto.crypto /home/crypto/gobyte -R
+chown crypto.crypto /home/crypto/.gobytecore -R
+
+source /home/crypto/gobyte/venv/bin/activate
+gobyted -daemon
+sudo cron
 echo "[$APP_NAME Server Started]"
 # tail -f /dev/null
